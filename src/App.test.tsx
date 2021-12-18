59769edbe1ef083887805bac7b6f7d06f93b821a
kripeshr22 <kranabha@students.pitzer.edu>
2021-12-17 23:03:20 -0800

    tests still not working

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,6 +6,9 @@ import { expect } from 'chai';
 import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import SignIn from "./components/SignIn";
+import MapDisplay from "./components/MapDisplay";
+import ResHallSelector from "./components/ResHallSelector";
+import FloorPlanSelector from "./components/FloorPlanSelector";
 
 describe('<App/> component test', () => {
   it('Sign In should render', () => {
@@ -20,6 +23,28 @@ describe('<App/> component test', () => {
     const loginComponent = shallow(<SignIn />);
     expect(loginComponent.contains('Sign In')).toBe(true);
   });
+
+  it('app should render', () => {
+    const AppComponent = shallow(<App/>);
+    AppComponent.setState({signedIn: 'j@j.com'})
+    const tree = toJson(AppComponent);
+    // eslint-disable-next-line jest/valid-expect,@typescript-eslint/no-unused-expressions
+    expect(tree).toMatchSnapshot;
+  })
+
+  it('map should display', () => {
+    const AppComponent = shallow(<App/>);
+    AppComponent.setState({signedIn: 'j@j.com'});
+    const MapComponent = shallow(<MapDisplay/>);
+    MapComponent.setState({resHalls: ['Drinkward', 'Linde']});
+    const ResHallComponent = shallow(<ResHallSelector/>);
+    ResHallComponent.setState({activeHall: 'drinkward'});
+    const FloorPlanComponent = shallow(<FloorPlanSelector/>);
+    FloorPlanComponent.setState({activeFloor: 1});
+    const tree = toJson(FloorPlanComponent);
+    // eslint-disable-next-line jest/valid-expect,@typescript-eslint/no-unused-expressions
+    expect(tree).toMatchSnapshot;
+  })
 
 
 })
